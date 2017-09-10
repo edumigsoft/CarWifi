f4ba6d134bf0ff3a19735d9ed3cdd7b7349cc625
Anderson <asa.sousa@gmail.com>
2017-09-10 16:26:33 -0300

    Teste com 3 saídas OK.

--- app/src/main/java/com/edumigrafa/carwifi/KotlinActivity.kt
+++ app/src/main/java/com/edumigrafa/carwifi/KotlinActivity.kt
@@ -11,14 +11,12 @@ class KotlinActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
 
-        text.setText("Lorem ipsum")
 
+        switch1.setOnClickListener {
 
-        button.setOnClickListener {
+            var led: String = if (switch1.isChecked) "1" else "0"
 
-            var led: String = if (button.isChecked) "1" else "0"
-
-            Fuel.get("http://10.0.1.1/" + led).response { request, response, result ->
+            Fuel.get("http://10.0.1.1/L1" + led).response { request, response, result ->
                 println(request)
                 println(response)
                 val (bytes, error) = result
@@ -26,11 +24,41 @@ class KotlinActivity : Activity() {
                     println(bytes)
                 }
 
-                text.setText(response.toString())
-
+                text.setText("Result = " + result.toString() + "\r" + "Response.Body = " + request.httpBody.toString())
             }
         }
 
+        switch2.setOnClickListener {
+
+            var led: String = if (switch2.isChecked) "1" else "0"
+
+            Fuel.get("http://10.0.1.1/L2" + led).response { request, response, result ->
+                println(request)
+                println(response)
+                val (bytes, error) = result
+                if (bytes != null) {
+                    println(bytes)
+                }
+
+                text.setText("Result = " + result.toString() + "\r" + "Response.Body = " + request.httpBody.toString())
+            }
+        }
+
+        switch3.setOnClickListener {
+
+            var led: String = if (switch3.isChecked) "1" else "0"
+
+            Fuel.get("http://10.0.1.1/L3" + led).response { request, response, result ->
+                println(request)
+                println(response)
+                val (bytes, error) = result
+                if (bytes != null) {
+                    println(bytes)
+                }
+
+                text.setText("Result = " + result.toString() + "\r" + "Response.Body = " + request.httpBody.toString())
+            }
+        }
 
     }
 
