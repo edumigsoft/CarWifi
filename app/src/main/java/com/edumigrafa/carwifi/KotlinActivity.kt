39b668add21dd03254cfb5d75b2acf0e0d26b6ef
Anderson <asa.sousa@gmail.com>
2017-09-10 21:35:33 -0300

    Teste inicial com acelerômetro, aceitável.

--- app/src/main/java/com/edumigrafa/carwifi/KotlinActivity.kt
+++ app/src/main/java/com/edumigrafa/carwifi/KotlinActivity.kt
@@ -1,12 +1,21 @@
 package com.edumigrafa.carwifi
 
 import android.app.Activity
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.KeyEvent
 import com.github.kittinunf.fuel.Fuel
 import kotlinx.android.synthetic.main.activity_kotlin.*
 
-class KotlinActivity : Activity() {
+class KotlinActivity() : Activity(), SensorEventListener {
+
+    val sensorManager: SensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,7 +61,7 @@ class KotlinActivity : Activity() {
                     println(bytes)
                 }
 
-                //text.setText("Result = " + result.success
+                //text.text("Result = " + result.success
                         //+ "\n"
                         //+ "Response = " + response.httpResponseMessage.toString()
                         //+ "\n"
@@ -73,7 +82,7 @@ class KotlinActivity : Activity() {
                     println(bytes)
                 }
 
-                text.setText("Result = " + result.toString() + "\r" + "Response.Body = " + request.httpBody.toString())
+                //text.text("Result = " + result.toString() + "\r" + "Response.Body = " + request.httpBody.toString())
             }
         }
 
@@ -89,7 +98,7 @@ class KotlinActivity : Activity() {
                     println(bytes)
                 }
 
-                text.setText("Result = " + result.toString() + "\r" + "Response.Body = " + request.httpBody.toString())
+                //text.text("Result = " + result.toString() + "\r" + "Response.Body = " + request.httpBody.toString())
             }
         }
 
@@ -98,20 +107,83 @@ class KotlinActivity : Activity() {
     fun action(led: String, act: String) {
 
         Fuel.get("http://10.0.1.1/" + led + act).response { request, response, result ->
-            println(request)
-            println(response)
-            println(result)
-            val (bytes, error) = result
-            if (bytes != null) {
-                println(bytes)
-            }
+            //println(request)
+            //println(response)
+            //println(result)
+            //val (bytes, error) = result
+            //if (bytes != null) {
+            //    println(bytes)
+            //}
 
-            //text.setText("Result = " + result.success
+            //text.text("Result = " + result.success
             //+ "\n"
             //+ "Response = " + response.httpResponseMessage.toString()
             //+ "\n"
             //+ "Request = " + request.toString()
             //)
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(
+                this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    var stable: Boolean = true;
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        text.text = event!!.values.zip("XYZ".toList()).fold("") { acc, pair ->
+            "$acc${pair.second}: ${pair.first}\n"
+        }
+
+        //text2.text = event!!.values.zip("Y".toList()).fold("Y") { acc, pair ->
+        //    "$acc${pair.second}: ${pair.first}\n"
+        //}
+
+        //text.text = event!!.values.zip("Y".toList()).fold("") { acc, pair ->
+        //    "$acc: ${pair.first}\n"
+        //}
+
+        //var str_ey: String = event!!.values.zip("X".toList()).fold("") { acc, pair ->
+        //    "$acc: ${pair.first}\n"
+        //}
+
+        //println(str_ey)
+
+        var float_ey: Float = event!!.values.get(1)
+        //println(float_ey)
+        text2.text = float_ey.toString()
+
+
+        if (float_ey > -1.0 && float_ey < 1.0) {
+            if (!stable) {
+                action("L1", "0")
+                action("L2", "0")
+                stable = true
+            }
+        }
+
+        if (float_ey > 1.0) {
+            action("L1", "1")
+            stable = false
+        }
+
+        if (float_ey < -1.0) {
+            action("L2", "1")
+            stable = false
         }
 
     }
